c9211993cfad750817bf244afa30b936bc38ec40
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2022-09-14 10:43:45 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, resp){
+    resp.send("Bem vindo ao meu app!");
+})
+
+app.get("/contato", function(req, resp){
+    resp.send("Página de contato");
+})
+
+app.get("/produto", function(req, resp){
+    resp.send("Página do produto")
+})
+
 //ultima linha
 app.listen(8081, function(){
     console.log("O servidor está funcionando na url http://localhost:8081");
