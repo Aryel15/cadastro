3450a3cbfdb55ceb81f19e72a85954f724f03142
Aryel15 <aannelouise@outlook.com>
2022-10-19 10:55:00 -0300

    19-10_c2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+//importar banco
+const db = require('./model/services/db.js');
 
 app.get("/", function(req, resp){
     resp.send("Bem vindo ao meu app!");
@@ -14,22 +16,23 @@ app.get("/produto", function(req, resp){
     resp.send("Página do produto")
 })
 
-//banco de dados
+//Banco de dados
 console.log('SELECT * FROM usuario');
-const usuarios = await db.selectUsuario();
+const usuarios = db.selectUsuario();
 console.log(usuarios);
 
 console.log('INSERT INTO usuario');
-const result = await db.insertUsuario({nome: "Zé", senha: "oi12345"});
+const result = db.insertUsuario({nome: "Zé", senha: "oi12345"});
 console.log(result);
 
 console.log('DELETE FROM usuario');
-const result3 = await db.deleteUsuario(2);
+const result3 = db.deleteUsuario(2);
 console.log(result3);
 
 console.log('UPDATE usuario');
-const result2 = await db.updateUsuario(3, {nome: "Zé José", senha: "oi12345"});
+const result2 = db.updateUsuario(3, {nome: "Zé José", senha: "oi12345"});
 console.log(result2);
+
 
 //ultima linha
 app.listen(8081, function(){
